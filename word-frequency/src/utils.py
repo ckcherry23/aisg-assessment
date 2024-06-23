3972f76531893f213112fbbd3a527c2504b870d4
Charisma Kausar <charisma.kausar@gmail.com>
2024-06-23 21:18:59 +0300

    feat: add util function to make ordinal

---
+++ word-frequency/src/utils.py
@@ -0,0 +1,9 @@
+def ordinal(n) -> str:
+    """
+    Convert an integer into its ordinal representation.
+    """
+    if 11 <= (n % 100) <= 13:
+        suffix = "th"
+    else:
+        suffix = ["th", "st", "nd", "rd", "th"][min(n % 10, 4)]
+    return str(n) + suffix
